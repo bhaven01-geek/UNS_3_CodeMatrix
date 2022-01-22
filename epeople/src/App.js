573fce5c23bdd575b9e7cc74e38fe2d488257996
bhaven01-geek <65948166+bhaven01-geek@users.noreply.github.com>
2022-01-22 17:30:41 +0530

    added login and home page

--- epeople/src/App.js
+++ epeople/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="dashboard/*" element={<Dashboard />} /> */}
+          {/* <Route path="test/*" element={<AboutTest />} /> */}
+          <Route exact path="/login" component={Login} />
+          {/* <Route path="quizStart" element={<TestPage />} /> */}
+        </Switch>
+      </BrowserRouter>
+   
+    </>
   );
 }
 
